f6a5345901529daceb447bfd4a5d690202181a53
Rodrigo Mendes <roodrigoomendessilva@gmail.com]>
2022-09-18 19:28:38 -0300

    :test_tube: test: Criando novo teste AluraPic CT0001.001 cadastro

---
+++ cypress/e2e/AluraPic/CT0002 Login.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress" />
+
+
+describe('CT0001 Cadastro', () => {
+    
+const EmailInvalido = 'teste.com'
+const EmailValido = 'teste@teste.com'
+const User = 'teste'
+
+    beforeEach(() => {
+        
+        cy.visit('http://alura-fotos.herokuapp.com');
+    })
+
+    
+
+})
