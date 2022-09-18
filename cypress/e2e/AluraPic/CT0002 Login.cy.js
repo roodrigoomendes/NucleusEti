d59f9ec6ca0c368fe82e0bd92cd2a00278542327
Rodrigo Mendes <roodrigoomendessilva@gmail.com]>
2022-09-18 19:52:16 -0300

    :test_tube: test: Criando novo teste Alura-CT0002 Login

--- cypress/e2e/AluraPic/CT0002 Login.cy.js
+++ cypress/e2e/AluraPic/CT0002 Login.cy.js
@@ -1,17 +1,48 @@
 /// <reference types="cypress" />
 
 
-describe('CT0001 Cadastro', () => {
+describe('CT0002 Login', () => {
     
-const EmailInvalido = 'teste.com'
-const EmailValido = 'teste@teste.com'
-const User = 'teste'
+const LoginValido = 'flavio'
+const LoginInvalido = 'rodrigo'
+const Senha = '123'
 
     beforeEach(() => {
-        
+        //DADO que estou na tela de login
         cy.visit('http://alura-fotos.herokuapp.com');
     })
 
+    it('001 - Fazendo login de usuario valido', () =>{
+        
+        //QUANDO Preencho o campo usuario valido
+        cy.get(':nth-child(1) > .form-control').type(LoginValido)
+        
+        //E Preencho o campo senha valida
+        cy.get(':nth-child(2) > .form-control').type(Senha)
+        
+        //E Tento acessar aplicação
+        cy.get('button[type="submit"]').click();
+
+        //ENTAO Consigo acessar a aplicação
+        cy.contains('a','(Logout)').should('be.visible');
+    })
+
+
+    it('002 - Fazendo login de usuario invalido', () =>{
+        
+        //QUANDO Preencho o campo usuario invalido
+        cy.get(':nth-child(1) > .form-control').type(LoginInvalido)
+        
+        //E Preencho o campo senha invalida
+        cy.get(':nth-child(2) > .form-control').type(Senha)
+        
+        //E Tento acessar aplicação
+        cy.get('button[type="submit"]').click();
+        
+        cy.on ('window:alert', (str) => {
+            expect(str).to.equal('Invalid user name or password')
+        })
+    })
     
 
 })
