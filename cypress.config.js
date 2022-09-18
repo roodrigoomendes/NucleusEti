4d9594e23b68c5276269f868ad93200553b24fa0
Rodrigo Mendes <roodrigoomendessilva@gmail.com]>
2022-09-18 20:42:19 -0300

    :books: Vinculando Cypress Dashboard para relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'rc9gaf',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
