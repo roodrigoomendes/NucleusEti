c8a2db1f2080f27f52792bd06d4863f76ce964c5
Rodrigo Mendes <roodrigoomendessilva@gmail.com]>
2022-09-18 21:06:41 -0300

    :books: Configuração mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'rc9gaf',
+  projectId: "rc9gaf",
   e2e: {
+    "reporter": "mochawesome",
+    "reporterOptions":{
+      "reportDir":"Cypress/reporte/mochawesome-report",
+      "overwrite": true,
+      "html": true,
+      "json": false,
+      "timestamp":"mmddyyyy_HHMMss"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
